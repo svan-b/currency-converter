3119cc80a030c9b1a895dad432172412a1c2ce87
svan-b <scott.vanbolhuis@gmail.com>
2024-01-07 23:09:07 -0800

    Initial commit with basic project structure and simple function

---
+++ currencyconverter/converter.py
@@ -0,0 +1,3 @@
+def convert_currency(amount, from_currency, to_currency):
+    # For now, let's pretend 1 unit of any currency is equal to 1 USD
+    return amount  # This will be replaced with actual conversion logic later
